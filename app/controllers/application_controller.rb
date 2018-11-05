58a33a0d3307f7bea735c44735763f2f32a057c8
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-05 02:48:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password])
     redirect '/users/home'
   end
 
