212af7e6ab20699d2ac688b1adc653e88d6a8497
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-05 02:36:06 +0000

    automatically backed up by learn

--- db/migrate/20181105023428_users.rb
+++ db/migrate/20181105023428_users.rb
@@ -3,6 +3,7 @@ class Users < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name 
       t.string :email 
-      t.string :password
+      t.string :password 
+    end
   end
 end
