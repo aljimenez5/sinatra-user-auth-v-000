d3a946e50b3b0bfd5da993c9b4ed7c64f8f0ef55
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-05 03:20:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    if params[:password] != @user.password
+    if @user == nil
       redirect '/sessions/login'
       raise "Incorrect email/password"
     session[:id] = @user.id
